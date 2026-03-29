004d3aafb22847eaefaa281e84f581691bcc951d
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-03-29 03:31:27 -0300

    add more data for user

--- your_data.py
+++ your_data.py
@@ -7,6 +7,8 @@ age = int(user_age)
 
 # Performance with inputs 
 years_to_100 = 100 - age
+average_lifepan_human = 73
+average_of_your_life = int((age / average_lifepan_human) * 100) 
 is_an_adult = age >= 18
 
 # Making output with f-string
@@ -14,6 +16,8 @@ output = f"""
 --- User Information: ---
 Hello {user_name}!
 You are {user_age} years old.
+The average lifepan of a human is {average_lifepan_human}.
+You has lived {average_of_your_life}% of the average.
 You will be 100 years old in {years_to_100} years.
 Are you an adult?: {is_an_adult}.
 --- End Of Information --- 
