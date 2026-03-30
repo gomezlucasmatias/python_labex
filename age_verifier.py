85cce3f456cdef689dc4795eb953082d6eed3e3b
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-03-30 20:58:30 -0300

    Implement adult age check: convert input to int and display result

--- age_verifier.py
+++ age_verifier.py
@@ -3,4 +3,7 @@ ask_for_age = input('How old are you? ')
 # Convert data input user
 age = int(ask_for_age)
 
-
+if age >= 18:
+    print(f"You are {age} old, You ara an adult")
+else:
+    print("You aren't an adult yet")
