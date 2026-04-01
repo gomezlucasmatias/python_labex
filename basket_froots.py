26de07371db8643545e42a5db1e1141d7ef6609a
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-01 16:58:44 -0300

    iterates through the characters of a string

--- basket_froots.py
+++ basket_froots.py
@@ -3,3 +3,8 @@ fruits = ['Apple', 'Banana', 'Pineapple', 'Strawberry', 'Lemon']
 # Iterate over basket contents 
 for fruit in fruits:
     print(f"I really like {fruit}")
+
+# Iterate character by character 
+for char in "Python":
+    print("\n")
+    print(char.upper())
