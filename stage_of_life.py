374053e3644a518b8efbc30f6e559b6f0e60bb5f
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-05 23:55:06 -0300

    Create If, elif and else structure

--- stage_of_life.py
+++ stage_of_life.py
@@ -4,4 +4,15 @@ age_user = input("Tell me how old you are?: ")
 # Transform input's user to a intiger 
 age = int(age_user)
 
-
+if age < 2:
+    print("You are a baby")
+elif age >= 2 and age < 4:
+    print("You are a toddler")
+elif age >= 4 and age < 13:
+    print("You are a kid")
+elif age >= 13 and age < 20:
+    print("You are a teenager")
+elif age >= 20 and age < 65:
+    print("You are an adult")
+else:
+    print("You are an elder")
