e02d9fc498afc2c6b0541d1fdd2fd012c7036200
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-07 21:50:00 -0300

    Calculator of price based quantities and discounts

---
+++ discount_calculator.py
@@ -0,0 +1,16 @@
+# Ask for quantities of computers and printers
+quantities_computers = input('How many computers do you want to buy? ')
+quantities_printers = input('How many printers do you want to buy? ')
+
+# Convert user inputs in integers
+computers = int(quantities_computers)
+printers = int(quantities_printers)
+
+# List of items price
+printers_cost = 2586
+computers_cost = 17458
+total = 
+
+# Process ticket
+if computers >= 10 and printers >= 10:
+    total == 
