9d11aa7ed42514ef1a1a041abe9cfbe65d2de39c
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-08 10:11:53 -0300

    A little game to practice with random

--- discount_calculator.py
+++ discount_calculator.py
@@ -17,3 +17,6 @@ printers_disc = 58
 if computers >= 10 and printers >= 10:
     total = (computers_cost * computers) + (printers_cost * printers) - (computers_disc * computers + printers_disc * printers)
     print(f"Your total price to pay is U$D {total}")
+elif computers >= 10 and printers < 10:
+    total = (computers_cost * computers) + (printers_cost * printers) - (computers_disc * computers)
+    print(f"Your total price to pay is U$D {total}")
