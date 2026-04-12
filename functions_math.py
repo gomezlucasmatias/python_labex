1ec3e9ee168f55ad9efe006361507a83eb153dea
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-12 13:53:27 -0300

    Created math functions with basic calculations

---
+++ functions_math.py
@@ -0,0 +1,22 @@
+# Function for import  
+
+# To sum two terms 
+def add(a, b):
+    return a + b
+
+# To rest two terms 
+def substract(a, b):
+    return a - b 
+
+# To multiply two terms 
+def multiply(a, b):
+    return a * b 
+
+# To divide two terms 
+def divide(a, b):
+    if b != 0:
+        return a / substract
+    else:
+        return 'Error: Division by zero' 
+
+PI = 3.14159
