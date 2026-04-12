3140219a2feae9d79c12de7f9d23bc6dd3ad761d
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-12 17:24:10 -0300

    import basics math functions to proobe

---
+++ use_functions_math.py
@@ -0,0 +1,14 @@
+# Import function crated
+
+import functions_math
+
+add = functions_math.add(15, 35)
+subtract = functions_math.subtract(75, 35)
+multiply = functions_math.multiply(12, 4)
+divide = functions_math.divide(75, 3)
+
+print(f"Addition: {add}")
+print(f"Subtraction: {subtract}")
+print(f"Multiplication: {multiply}")
+print(f"Division: {divide}")
+print(f"Value of PI: {functions_math.PI}")
