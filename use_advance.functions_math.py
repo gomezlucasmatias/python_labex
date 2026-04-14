928497c9001f01546cbac328da7a2ca4a82d5798
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-13 21:55:46 -0300

    To practice import functions created

---
+++ use_advance.functions_math.py
@@ -0,0 +1,14 @@
+# Import advance_functions_math
+
+from advance_functions_math import square_root, power
+from advance_functions_math import sin as sine, cos as cosine
+
+x = 16
+y = 2
+angle = 30
+
+print(f"Square root of {x}: {square_root(x)}")
+print(f"{x} to the power of {y}: {power(x, y)}")
+print(f"Sine of {angle} degrees: {sine(angle)}")
+print(f"Cosine of {angle} degrees: {cosine(angle)}")
+
