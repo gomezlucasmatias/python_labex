b96554a7a0f1680eae07d08dc48f15fb62111a24
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-13 21:28:57 -0300

    Add Geometry functions

--- advance_functions_math.py
+++ advance_functions_math.py
@@ -1,5 +1,20 @@
 import math 
 
+# Square root function
 def square_root(x):
     return math.sqrt(x)
+
+# Power function
+def power(base, exponent):
+    return math.pow(base, exponent)
+
+# Sin function
+def sin(angle):
+    return math.sin(math.radians(angle))
+
+# Cos function
+def cos(angle):
+    return math.cos(math.radians(angle))
+
+
 
