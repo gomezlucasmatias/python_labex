59eb398728d41b859702a43042a3177b4ccea73a
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-20 22:46:14 -0300

    Improve comentary

--- list_like_pro.py
+++ list_like_pro.py
@@ -1,6 +1,6 @@
-# Create t# Prints the multiplication table of a user-selected number up to 10
+# Prints the multiplication table of a user-selected number up to 10
 table = int(input("Whata multiplicatioin table do you want to know?: "))
 
 list = [table*i for i in range(1, 11)]
 
-print(list)
+print(f"")
