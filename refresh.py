6f74c89509717479e1f063edc6abf88de03ca302
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-21 23:28:32 -0300

    Improve format

--- refresh.py
+++ refresh.py
@@ -3,6 +3,8 @@ weigth = int(input("Put your weigth in kilograms please: "))
 libras = weigth * 2.204
 
 print(f"""
+      #################################
       *** Your weigth in pounds is: ***
-      {round(libras)} lb
+      **********{round(libras)} lb*******
+      #################################
       """)
