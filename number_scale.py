a84535a585e67baaed50247256a942dc7c9409b4
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-27 23:05:32 -0300

    Conditional works ok

---
+++ number_scale.py
@@ -0,0 +1,10 @@
+# Ask for a number
+number = int(input("Dame un numero y yo te voy a decir si este es positivo, negativo o cero "))
+
+# Compare number
+if number > 0:
+    print("Tu numero es positivo")
+elif number < 0:
+    print("Tu numero es negativo")
+else:
+    print("Tu numero es 0")
