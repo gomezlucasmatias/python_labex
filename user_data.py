05f264f1abac0ef41a14454b31fd7198c757ad31
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-04-27 21:48:52 -0300

    A little program to procces user inputs

---
+++ user_data.py
@@ -0,0 +1,9 @@
+nombre = input("Señorita de la galera extravagante, digame como me puedo referir a usted? ")
+edad = input(f"Oh {nombre}, hermoso nombre el que tiene usted, que edad tiene bella dama? ")
+
+numero = int(input(f"""
+Mi querida {nombre}, le propongo un juego en el que usted me dice un número
+y yo le devuelvo el doble de este, adelante, brindeme un número, cualquiera sea 
+"""))
+
+print(f"Pues dejeme decirle me bellísima {nombre} que el doble de {numero} es {numero * 2}")
