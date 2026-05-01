55069c04d1b382c236473416fb161c7723cef384
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-05-01 00:45:58 -0300

    Add error message

--- imc.py
+++ imc.py
@@ -2,16 +2,17 @@
 peso = float(input("Escribe tu peso en kg: "))
 altura = float(input("Escribe tu altura en metros: "))
 
-if peso > 0 and altura > 0 :
-    imc = peso / (altura * altura)
+if peso > 0 and altura > 0:
+    imc = round(peso / (altura * altura), 2)
+    print(f"tu imc es {imc}")
 
-print(imc)
-
-if imc < 18.5 :
-    print(f"Tienes un imc de {imc}, es bajo")
-elif imc > 18.5 and imc < 25 :
-    print(f"Tienes un imc de {imc}, es normal")
-elif imc > 25 and imc < 30 :
-    print(f"Tienes un imc de {imc}, tienes sobrepeso")
+    if imc < 18.5:
+        print(f"Tienes un imc de {imc}, es bajo")
+    elif imc < 25:
+        print(f"Tienes un imc de {imc}, es normal")
+    elif imc < 30:
+        print(f"Tienes un imc de {imc}, tienes sobrepeso")
+    else:
+        print(f"Tienes un imc de {imc}, eres obeso")
 else:
-    print(f"Tienes un imc de {imc}, eres obeso")
+    print("Debes ingresar Peso y Altura mayores a 0")
