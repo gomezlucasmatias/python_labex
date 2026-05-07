a4efa63549e61e802b15cf2cf0a2fabc2a948e5d
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-05-06 22:34:28 -0300

    Complete first list exercise

---
+++ game_with_list.py
@@ -0,0 +1,10 @@
+# Define an empty list
+empty = []
+
+# Define something
+something = "something"
+
+# Fill the list with something
+for letra in something:
+    empty.append(letra)
+    print(letra.upper())
