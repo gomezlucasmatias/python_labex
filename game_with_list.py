aadc2e6590d407cc3da040e43272b779ac5d7782
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-05-07 23:46:04 -0300

    Improve

--- game_with_list.py
+++ game_with_list.py
@@ -7,16 +7,16 @@ something = input("Give your favorite word to see if it hides a small robot in i
 # Fill the list with something
 for letra in something.lower():
     empty.append(letra)
-    print(letra.upper())
-    if letra == 'h':
-        print('[`w`]')
-    elif letra == 's':
-        print(']º_º[')
-    elif letra == 'a':
-        print("{*-*}")
-    elif letra == 'e':
-        print("('__')")
-    elif letra == 'd':
-        print("|o.o|")
+    for letra in empty:
+        if letra == 'h':
+            print('[`w`]')
+        elif letra == 's':
+            print(']º_º[')
+        elif letra == 'a':
+            print("{*-*}")
+        elif letra == 'e':
+            print("('__')")
+        elif letra == 'd':
+            print("|o.o|")
     
         
