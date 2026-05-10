ff56307f27b9e08a45cc280a6700a601248ee57a
Gomez Lucas Matias <lucasmatias1.0@yahoo.com>
2026-05-10 16:41:52 -0300

    Fixed wrong code

--- fibonachi.py
+++ fibonachi.py
@@ -1,11 +1,11 @@
 # Attempt
 
-prev_num = 0
-next_num = 1
+sum_1 = 1
+sum_2 = 1
 
 for i in range(1, 20):
-    num = prev_num + next_num
-    print(f"{prev_num} plus {next_num} equal {num}")
-    prev_num = next_num
-    next_num = next_num + num
+    num = sum_1 + sum_2
+    print(f"{sum_1} + {sum_2} = {num}")
+    sum_1 = sum_2
+    sum_2 = num
 
